7e629b97efb403c2d1c594737d8d23412a4f663d
Guido García <guido.garciabernardo@telefonica.com>
2021-01-14 15:22:57 +0100

    chore: demo

--- crawl.sh
+++ crawl.sh
@@ -4,5 +4,12 @@ SITES=`cat sites.csv | cut -d ',' -f 1`
 for site in $SITES; do
   # see https://github.com/mozilla/http-observatory/blob/master/httpobs/docs/api.md
   echo "Scanning $site using Mozilla HTTP Observatory API"
-  curl -s -X POST "https://http-observatory.security.mozilla.org/api/v1/analyze?host=$site&rescan=true" > output/${site//./¡}.json
+  curl -s -X POST "https://http-observatory.security.mozilla.org/api/v1/analyze?host=$site&rescan=true"
+done
+
+sleep 60
+
+for site in $SITES; do
+  echo "Requesting $site scan results"
+  curl -s -X GET "https://http-observatory.security.mozilla.org/api/v1/analyze?host=$site" > output/${site//./¡}.json
 done
