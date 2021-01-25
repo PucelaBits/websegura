95a350f5f6c56fd9158e6d490a82913d48263555
Adrián de la Rosa Martín <adrian@adrm.es>
2021-01-25 13:06:35 +0100

    Sitios de CyL separados. Tabla abstraida como macro para evitar repetirla.

--- crawl.sh
+++ crawl.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-SITES=`cat _data/provincias/valladolid.json | jq -r .webs[].url`
-echo $SITES;
+SITES_VA=`cat _data/provincias/valladolid.json | jq -r .webs[].url`
+SITES_CYL=`cat _data/comunidades/castilla-y-leon.json | jq -r .webs[].url`
+SITES="${SITES_VA}\n${SITES_CYL}"
+echo "$SITES";
 for site in $SITES; do
   # see https://github.com/mozilla/http-observatory/blob/master/httpobs/docs/api.md
   echo "Scanning $site using Mozilla HTTP Observatory API"
