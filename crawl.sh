cc5581ed4f093aafddfd51cebe479b860baeeed1
nukeador <nukeador@protonmail.com>
2021-02-02 14:06:07 +0100

    Primera impementación de historial

--- crawl.sh
+++ crawl.sh
@@ -32,8 +32,11 @@ done
 
 sleep 60  # XXX polling until mozilla finishes scanning the site might be more elegant
 rm _data/results/*.json
+rm _data/results/history/*.json
 
 for site in $SITES; do
+  echo "Requesting $site scan history"
+  curl -s -X GET "https://http-observatory.security.mozilla.org/api/v1/getHostHistory?host=$site" | jq . > _data/results/history/${site//./!}.json
   echo "Requesting $site scan results"
   curl -s -X GET "https://http-observatory.security.mozilla.org/api/v1/analyze?host=$site" | jq . > _data/results/${site//./!}.json
 done
