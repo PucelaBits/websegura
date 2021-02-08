bb01bdf5f594ae569d2992022584a10273db534d
Guillermo López <guillemo.lopezleal@vonage.com>
2021-02-08 13:49:53 +0100

    Add duplicates test with ghactions

--- crawler/sites-parser.js
+++ crawler/sites-parser.js
@@ -8,16 +8,23 @@ const glob = require('fast-glob');
 const MAX_TIME_TO_REFRESH_MILLIS = 3 * 24 * 60 * 60 * 1000; // 3 days, can be increased when we have many sites to scan
 
 /**
- * Obtiene los ficheros global, de comunidades y provincias y devuelve la URL
- * de aquellos que no se han refrescado en los últimos MAX_TIME_TO_REFRESH_MILLIS.
+ * Obtiene los ficheros global, de comunidades y provincias.
+ */
+function getAllUrls() {
+  const files = glob.sync('_data/{comunidades,provincias}/*.json');
+  files.push('_data/general.json')
+  return files
+    .flatMap(file => JSON.parse(fs.readFileSync(file)).webs.map(x => beautify(x.url)))
+}
+
+/**
+ * Devuelve la URL de las webs que no se han refrescado
+ * en los últimos MAX_TIME_TO_REFRESH_MILLIS.
  *
  * For the sake of simplicity, this function is sync for now
  */
 async function parse() {
-  const files = glob.sync(['_data/{comunidades,provincias}/*.json', '_data/general.json']);
-  return files
-    .flatMap(file => JSON.parse(fs.readFileSync(file)).webs.map(x => beautify(x.url)))
-    .filter(outdated);
+  return getAllUrls().filter(outdated);
 }
 
 // Mozilla espera un hostname (sin / final y sin indicar protocolo "http[s]://")
@@ -46,5 +53,7 @@ function outdated(site) {
 }
 
 module.exports = {
-  parse: parse
+  parse: parse,
+  beautify: beautify,
+  getAllUrls: getAllUrls,
 }
