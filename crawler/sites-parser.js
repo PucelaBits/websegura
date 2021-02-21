d1e25c4d7c3dca3c61ea37813128d5ef74d73575
Adrián de la Rosa Martín <adrian@adrm.es>
2021-02-21 14:06:59 +0100

    Days to refresh extracted to new constant. Prettier.js fixes.

--- crawler/sites-parser.js
+++ crawler/sites-parser.js
@@ -2,20 +2,22 @@
  * Fichero con utilidades comunes al crawling, para procesar los ficheros de entrada
  * gloal, de comunidades y de provincias.
  */
-const fs = require('fs');
-const glob = require('fast-glob');
+const fs = require("fs");
+const glob = require("fast-glob");
 
-const MAX_TIME_TO_REFRESH_MILLIS = 2 * 24 * 60 * 60 * 1000; // 3 days, can be increased when we have many sites to scan
+const MAX_DAYS_TO_REFRESH = 2; // can be increased when we have many sites to scan
+const MAX_TIME_TO_REFRESH_MILLIS = MAX_DAYS_TO_REFRESH * 24 * 60 * 60 * 1000;
 const MAX_RESULTS = 200;
 
 /**
  * Obtiene los ficheros global, de comunidades y provincias.
  */
 function getAllUrls() {
-  const files = glob.sync('_data/{comunidades,provincias}/*.json');
-  files.push('_data/general.json')
-  return files
-    .flatMap(file => JSON.parse(fs.readFileSync(file)).webs.map(x => beautify(x.url)))
+  const files = glob.sync("_data/{comunidades,provincias}/*.json");
+  files.push("_data/general.json");
+  return files.flatMap((file) =>
+    JSON.parse(fs.readFileSync(file)).webs.map((x) => beautify(x.url))
+  );
 }
 
 /**
@@ -25,7 +27,7 @@ function getAllUrls() {
  *
  * For the sake of simplicity, this function is sync for now
  */
-async function parse(limit=MAX_RESULTS) {
+async function parse(limit = MAX_RESULTS) {
   // XXX applying the limit during the filtering phase would
   //     be more efficient, but js sucks sometimes
   return getAllUrls().filter(outdated).slice(0, limit);
@@ -33,19 +35,21 @@ async function parse(limit=MAX_RESULTS) {
 
 // Mozilla espera un hostname (sin / final y sin indicar protocolo "http[s]://")
 function beautify(url) {
-  url = url.replace('http://', '');
-  url = url.replace('https://', '');
+  url = url.replace("http://", "");
+  url = url.replace("https://", "");
   return new URL(`https://${url}`).hostname;
 }
 
 function outdated(site) {
-  const fileName = site.replace(/\./g, '!') + '.json';
+  const fileName = site.replace(/\./g, "!") + ".json";
   const path = `_data/results/${fileName}`;
 
   try {
     const siteInfo = JSON.parse(fs.readFileSync(path));
-    const recent = new Date(siteInfo.start_time).valueOf() > Date.now() - MAX_TIME_TO_REFRESH_MILLIS;
-    if (siteInfo.state === 'FINISHED' && recent) {
+    const recent =
+      new Date(siteInfo.start_time).valueOf() >
+      Date.now() - MAX_TIME_TO_REFRESH_MILLIS;
+    if (siteInfo.state === "FINISHED" && recent) {
       return false;
     }
   } catch (err) {
@@ -59,4 +63,4 @@ module.exports = {
   parse: parse,
   beautify: beautify,
   getAllUrls: getAllUrls,
-}
+};
